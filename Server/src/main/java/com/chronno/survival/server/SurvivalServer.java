bb4376b32e512a30f8fc54a9bee272df328f8746
chronno <quinterosmanu@gmail.com>
2021-04-30 01:17:11 -0300

    Small code cleanup

--- Server/src/main/java/com/chronno/survival/server/SurvivalServer.java
+++ Server/src/main/java/com/chronno/survival/server/SurvivalServer.java
@@ -1,48 +1,25 @@
 package com.chronno.survival.server;
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.Game;
 import com.chronno.survival.network.server.GameServer;
 
 
-public class SurvivalServer implements ApplicationListener {
+public class SurvivalServer extends Game {
 
     private final GameServer server;
-    private TiledMap map;
+
 
     public SurvivalServer() {
         server = new GameServer(new ServerNetworkInterpreter());
-        server.run();
     }
 
     @Override
     public void create()  {
-        map = new TmxMapLoader().load("maps/example.tmx");
-    }
-
-    @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void render() {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
+        server.run();
     }
 
     @Override
     public void dispose() {
-
+        server.getServer().stop();
     }
 }
